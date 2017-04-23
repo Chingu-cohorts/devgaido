72f371c6c6f03eaf6c4f9e65aa5da84379c29177
Kim Kwanka <kimkwanka@gmail.com>
2017-04-23 18:28:04 +0200

    Added basic routing functionality. TODO: server-side

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route, Switch } from 'react-router';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 import Home from './pages/Home';
@@ -12,13 +12,19 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>I AM APP!</h1>
-        <Router history={createBrowserHistory()}>
-          <Switch>
-            <Route path="/" component={Home} />
-            <Route path="/login" component={Login} />
-          </Switch>
-        </Router>
+        <h1>I AM APP!</h1>        
+        <BrowserRouter history={createBrowserHistory()}>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/login">Login</Link></li>
+            </ul>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
