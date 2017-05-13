65039b5524ec9120e890f5b00e54c9145bd61d00
mrhos <erikhos@outlook.com>
2017-05-13 19:36:38 +0200

    updated style, header, login and menu links

--- src/client/pages/shared/Header.jsx
+++ src/client/pages/shared/Header.jsx
@@ -44,43 +44,40 @@ class Header extends React.Component {
   }
   render() {
     return (
-      <header>
-        <div>
-          <a href="#" onClick={e => handleClick(e)}>Login</a>
-        </div>
-        <div>
-          <a href="/logout">Logout</a>
-        </div>
-        <div className="menu">
-          <a className="menu-btn" href="#" onClick={e => this.toggleMenu(e)} >Menu</a>
-        </div>
-        <div className="search-header">
-          <label className="search">
-            <input type="text" name="search" placeholder="Search" />
-          </label>
-        </div>
+      <div>
+        <header>
+          <div className="header-content">
+            <div className="menu">
+              <a className="menu-btn" href="#" onClick={e => this.toggleMenu(e)} title="Menu"></a>
+              <a className="login" href="#" onClick={e => handleClick(e)} title="Login"></a>
+              <a className="hidden" href="/logout" title="Logout"></a>
+            </div>
+            <div className="search-header">
+              <label className="search">
+                <input type="text" name="search" placeholder="Search" />
+              </label>
+            </div>
+          </div>
+        </header>
         <div className={this.menuClass}>
-          <div className="panel-links">
-            <a className="side-panel-close" href="#">Close</a>
-          </div>
-          <div className="panel-menu">
-            <h1>Welcome back Erik!</h1>
+            <div className="panel-menu">
+              <h1>Welcome back Erik!</h1>
 
-            <h2>Your learning path</h2>
-            <ul>
-              <li className="times"><i className="fa fa-calendar" aria-hidden="true" />Schedule</li>
-              <li className="times"><i className="fa fa-graduation-cap" aria-hidden="true" />Lessons</li>
-              <li><i className="fa fa-book" aria-hidden="true" />Resources</li>
-            </ul>
-            <h2>Your Settings</h2>
-            <ul>
-              <li className="information"><i className="fa fa-user-o" aria-hidden="true" />Your profile</li>
-              <li><i className="fa fa-cog" aria-hidden="true" />Preferences</li>
-              <li><i className="fa fa-flag" aria-hidden="true" />Notifications</li>
-            </ul>
+              <h2>Your learning path</h2>
+              <ul>
+                <li className="times"><i className="fa fa-calendar" aria-hidden="true" />Schedule</li>
+                <li className="times"><i className="fa fa-graduation-cap" aria-hidden="true" />Lessons</li>
+                <li><i className="fa fa-book" aria-hidden="true" />Resources</li>
+              </ul>
+              <h2>Your Settings</h2>
+              <ul>
+                <li className="information"><i className="fa fa-user-o" aria-hidden="true" />Your profile</li>
+                <li><i className="fa fa-cog" aria-hidden="true" />Preferences</li>
+                <li><i className="fa fa-flag" aria-hidden="true" />Notifications</li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </header>
+      </div>
 
     );
   }
