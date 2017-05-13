5d15ab0d0c6a58ffa5306cadf3ad1563ac712ec9
jdmedlock <jdmedlock@gmail.com>
2017-05-13 13:59:30 -0500

    Removed FCC icon from footer

--- src/client/pages/shared/Footer.jsx
+++ src/client/pages/shared/Footer.jsx
@@ -4,8 +4,10 @@ const Footer = () =>
   (
     <footer>
       <div className="footer-content">
-        <h5><a href="https://chingu-cohorts.github.io/chingu-directory/" target="_blank">
-        <i class="fa fa-fire"></i>&copy; Chingu Cohorts, 2017</a></h5>
+        <h5>
+          <a href="https://chingu-cohorts.github.io/chingu-directory/" target="_blank">
+            &copy; Chingu Cohorts, 2017</a>
+        </h5>
       </div>
     </footer>
   );
