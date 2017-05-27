12357e176f54c4098be2ee78afdeb97663bb43fd
Jim Medlock <jdmedlock@gmail.com>
2017-05-27 16:15:48 -0500

    Add test for coreCourses.getAllCourses

--- test/testCourse.Spec.js
+++ test/testCourse.Spec.js
@@ -3,8 +3,27 @@ const coreCourses = require('../src/server/services/coreCourses');
 
 const assert = require('assert');
 
-describe('getCourse', function () {
-  it('should return the named course - "Understanding & Working with Git"', function () {
-    assert.equal('Understanding & Working with Git', coreCourses.getCourse('10000').name);
+/**
+ * Test the coreCourses.getCourse function
+ */
+describe('Test coreCourses.js functions', function () {
+  describe('Test the getCourse function', function () {
+    it('should return the named course - "Understanding & Working with Git"', function () {
+      assert.equal('Understanding & Working with Git', coreCourses.getCourse('10000').name);
+    });
+    it('should return undefined for an unknown course id', function () {
+      assert.equal(undefined, coreCourses.getCourse('99999'));
+    });
+  });
+});
+
+/**
+ * Test the coreCourses.getCourse function
+ */
+describe('Test coreCourses.js functions', function () {
+  describe('Test the getAllCourses function', function () {
+    it('should return multiple courses"', function () {
+      assert.equal(coreCourses.getAllCourses() > 0, coreCourses.getAllCourses().length > 0);
+    });
   });
 });
