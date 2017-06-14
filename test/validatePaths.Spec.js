6694a561d5707e9ffdebd259cab083c08f4a1259
Jim Medlock <jdmedlock@gmail.com>
2017-06-13 20:11:29 -0500

    Complete validation test for corepaths.json data

--- test/validatePaths.Spec.js
+++ test/validatePaths.Spec.js
@@ -2,6 +2,7 @@
 require('./registerBabel');
 
 import corePaths from '../src/server/models/corepaths.json';
+import coreCourses from '../src/server/models/corecourses.json';
 
 const assert = require('assert');
 
@@ -9,26 +10,57 @@ const assert = require('assert');
  * Check the validity of the corepaths.json file
  */
 describe('Validate corepaths.json', function () {
-  describe('Validate path ids', function () {
+  describe('Validate path id length', function () {
+    let invalidPathIds = [];
+    afterEach(function() {
+      invalidPathIds.forEach((id) => {
+        console.log(`         Path id ${id} > 16 characters`);
+      });
+      invalidPathIds = [];
+    });
     it('should verify that path ids are <= 16 characters', function () {
-      const invalidPathIds = [];
       Object.keys(corePaths).forEach((id) => {
         if (id.length > 16) {
-          console.log(`Path id ${id} > 16 characters`);
           invalidPathIds.push(id);
         }
       });
       assert.equal(invalidPathIds.length, 0);
     });
-    it('should verify that path ids contain only letters and digits', function () {
-      const invalidPathIds = [];
+  });
+  describe('Validate path id composition', function () {
+    let invalidPathIds = [];
+    afterEach(function() {
+      invalidPathIds.forEach((id) => {
+        console.log(`         Path id ${id} contains invalid characters`);
+      });
+      invalidPathIds = [];
+    });
+    it('should verify that path ids contain only lowercase letters and digits', function () {
       Object.keys(corePaths).forEach((id) => {
         if (!id.match(/^[0-9a-z]+$/)) {
-          console.log(`Path id ${id} not limited to just letters and digits`);
           invalidPathIds.push(id);
         }
       });
       assert.equal(invalidPathIds.length, 0);
+    });
+  });
+  describe('Validate course ids in the path exists', function () {
+    let invalidIds = [];
+    afterEach(function() {
+      invalidIds.forEach((id) => {
+        console.log(`         Path id ${id[0]} contains unknown course id ${id[1]}`);
+      });
+      invalidIds = [];
+    });
+    it('should verify that course ids exist', function () {
+      Object.values(corePaths).forEach((currentPath) => {
+        currentPath["courseIds"].forEach((courseId) => {
+          if (coreCourses[courseId] === undefined) {
+            invalidIds.push([currentPath["id"], courseId]);
+          }
+        });
+      });
+      assert.equal(invalidIds.length, 0);
     });
   });
 });
