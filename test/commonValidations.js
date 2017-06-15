20b65e0adbe6b7339d14bd7cce5152905da4fb9f
jdmedlock <jdmedlock@gmail.com>
2017-06-15 18:58:53 -0500

    Refactor to add common validation routines to eliminate duplicated logic

---
+++ test/commonValidations.js
@@ -0,0 +1,55 @@
+/**
+ * Validate the composition of a path, course, lesson identifier
+ *
+ * @param {Object} jsonData - JSON object containing the data elements. This must be formatted as {"<id>": {...id: "<id>"...}...}
+ * @returns {String[]} invalidIds - Array of invalid id's. Those containing something other than lowercase letters and digits.
+ */
+const validateIdComposition = (jsonData) => {
+  const invalidIds = [];
+  Object.keys(jsonData).forEach((itemId) => {
+    if (!itemId.match(/^[0-9a-z]+$/)) {
+      invalidIds.push(itemId);
+    }
+  });
+  return invalidIds;
+};
+
+/**
+ * Validate the length of a path, course, lesson identifier
+ *
+ * @param {Object} jsonData - JSON object containing the data elements. This must be formatted as {"<id>": {...id: "<id>"...}...}
+ * @returns {String[]} invalidIds - Array of id's exceeding 16 characters
+ */
+const validateIdLength = (jsonData) => {
+  const invalidIds = [];
+  Object.keys(jsonData).forEach((itemId) => {
+    if (itemId.length > 16) {
+      invalidIds.push(itemId);
+    }
+  });
+  return invalidIds;
+};
+
+/**
+ * Validate a relationship between two data elements.
+ *
+ * @param {String} childAttrNm - Attribute name in the child JSON object
+ * @param {Object} childJSON - JSON object containing the child data elements. This must be formatted as {"<id>": {...id: "<id>"...}...}
+ * @param {String} parentAttrNm - Attribute name in the child JSON object
+ * @param {Object} parentJSON - JSON object containing the parent data elements. This must be formatted as {"<id>": {...id: "<id>"...}...}
+ * @returns {String[]} invalidIds - Array of id's exceeding 16 characters
+ */
+const validateRelationship = (childAttrNm, childJSON, parentAttrNm, parentJSON) => {
+  const invalidIds = [];
+
+  Object.values(childJSON).forEach((currentPath) => {
+    currentPath[childAttrNm].forEach((itemId) => {
+      if (parentJSON[itemId] === undefined) {
+        invalidIds.push([currentPath[parentAttrNm], itemId]);
+      }
+    });
+  });
+  return invalidIds;
+};
+
+export { validateIdComposition, validateIdLength, validateRelationship };
--- test/validatePaths.Spec.js
+++ test/validatePaths.Spec.js
@@ -1,6 +1,7 @@
-/* eslint-disable func-names prefer-arrow-callback */
+/* eslint-disable func-names prefer-arrow-callback no-console */
 require('./registerBabel');
 
+import { validateIdComposition, validateIdLength, validateRelationship } from './commonValidations';
 import corePaths from '../src/server/models/corepaths.json';
 import coreCourses from '../src/server/models/corecourses.json';
 
@@ -9,57 +10,43 @@ const assert = require('assert');
 /**
  * Check the validity of the corepaths.json file
  */
-describe('Validate corepaths.json', function () {
-  describe('Validate path id length', function () {
+describe('Validate corepaths.json', () => {
+  describe('Validate path id length', () => {
     let invalidPathIds = [];
-    afterEach(function() {
+    afterEach(() => {
       invalidPathIds.forEach((id) => {
         console.log(`${' '.repeat(9)}Path id ${id} > 16 characters`);
       });
       invalidPathIds = [];
     });
-    it('should verify that path ids are <= 16 characters', function () {
-      Object.keys(corePaths).forEach((id) => {
-        if (id.length > 16) {
-          invalidPathIds.push(id);
-        }
-      });
+    it('should verify that path ids are <= 16 characters', () => {
+      invalidPathIds = validateIdLength(corePaths);
       assert.equal(invalidPathIds.length, 0);
     });
   });
-  describe('Validate path id composition', function () {
+  describe('Validate path id composition', () => {
     let invalidPathIds = [];
-    afterEach(function() {
+    afterEach(() => {
       invalidPathIds.forEach((id) => {
         console.log(`${' '.repeat(9)}Path id ${id} contains invalid characters`);
       });
       invalidPathIds = [];
     });
-    it('should verify that path ids contain only lowercase letters and digits', function () {
-      Object.keys(corePaths).forEach((id) => {
-        if (!id.match(/^[0-9a-z]+$/)) {
-          invalidPathIds.push(id);
-        }
-      });
+    it('should verify that path ids contain only lowercase letters and digits', () => {
+      invalidPathIds = validateIdComposition(corePaths);
       assert.equal(invalidPathIds.length, 0);
     });
   });
-  describe('Validate course ids in the path exists', function () {
+  describe('Validate course ids in the path exists', () => {
     let invalidIds = [];
-    afterEach(function() {
+    afterEach(() => {
       invalidIds.forEach((id) => {
         console.log(`${' '.repeat(9)}Path id ${id[0]} contains unknown course id ${id[1]}`);
       });
       invalidIds = [];
     });
-    it('should verify that course ids exist', function () {
-      Object.values(corePaths).forEach((currentPath) => {
-        currentPath["courseIds"].forEach((courseId) => {
-          if (coreCourses[courseId] === undefined) {
-            invalidIds.push([currentPath["id"], courseId]);
-          }
-        });
-      });
+    it('should verify that course ids exist', () => {
+      invalidIds = validateRelationship('courseIds', corePaths, 'courseId', coreCourses);
       assert.equal(invalidIds.length, 0);
     });
   });
