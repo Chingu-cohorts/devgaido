eee37aaf7a5505a78df823f42c4b3a2db463e0d1
jdmedlock <jdmedlock@gmail.com>
2017-06-18 07:54:43 -0500

    Add validation for orphaned courses; Correct test descriptions

--- test/validateCourses.Spec.js
+++ test/validateCourses.Spec.js
@@ -1,10 +1,11 @@
 /* eslint-disable func-names no-console */
 require('./registerBabel');
 
-import { logInvalidIds, logInvalidRelations, 
+import { logInvalidIds, logInvalidRelations,
   validateIdComposition, validateIdLength, validateIdMatch, validateRelationship } from './commonValidations';
 import coreCourses from '../src/server/models/corecourses.json';
 import coreLessons from '../src/server/models/corelessons.json';
+import corePaths from '../src/server/models/corepaths.json';
 
 const assert = require('assert');
 
@@ -50,6 +51,28 @@ describe('Validate corecourses.json', () => {
     it('should verify that lesson ids exist', () => {
       invalidIds = validateRelationship('lessonIds', coreCourses, 'lessonId', coreLessons);
       assert.equal(invalidIds.length, 0);
+    });
+  });
+  describe('Vaidate that there are no orphaned lessons', () => {
+    let orphanedCourseIds = [];
+    const allPathCourses = Object.values(corePaths).reduce((courseReferences, path) => {
+      path.courseIds.forEach((courseId) => {
+        if (courseReferences.indexOf(courseId) === -1) {
+          courseReferences.push(courseId);
+        }
+      });
+      return courseReferences;
+    }, []);
+    afterEach(() => {
+      orphanedCourseIds = logInvalidIds(orphanedCourseIds, 'Course id not referenced by any path');
+    });
+    it('should verify that each course id is referenced by at least one path', () => {
+      Object.values(coreCourses).forEach((currentCourse) => {
+        if (allPathCourses.indexOf(currentCourse.id) === -1) {
+          orphanedCourseIds.push(currentCourse.id);
+        }
+      });
+      assert.equal(orphanedCourseIds.length, 0);
     });
   });
 });
