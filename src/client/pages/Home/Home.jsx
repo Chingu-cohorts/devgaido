6469524e83289845f857b14c3d616e502472427d
Kim Kwanka <kimkwanka@gmail.com>
2017-06-22 13:43:33 +0200

    Change landing page tagline.

--- src/client/pages/Home/Home.jsx
+++ src/client/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => (
     <div className="home-hero">
       <div className="home-content">
         <h1 className="hero-tagline">LEARN WEB DEVELOPMENT</h1>
-        <h1 className="hero-tagline hero-tagline-colored">THE GUIDED WAY</h1>
+        <h1 className="hero-tagline hero-tagline-colored">THE DEVGAIDO WAY</h1>
         <div className="hero-details">
           <h2>Whether you&#039;re just starting out or want to brush up a certain skill:</h2>
           <h2><span className="hero-tagline-colored">DevGaido</span> provides easy to follow learning paths that help you reach your goal without the hassle.</h2>
