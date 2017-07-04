471930bd17e94da364585b4c4c21e889ead3db8a
Kim Kwanka <kimkwanka@gmail.com>
2017-07-04 20:12:20 +0200

    Add improved logo to Header and Auth0LockWidget.

--- src/client/pages/shared/Header.jsx
+++ src/client/pages/shared/Header.jsx
@@ -21,14 +21,7 @@ const toggleMenuAfterNav = (dispatch) => {
 const Header = ({ user, uiState, dispatch, lock }) => (
   <header>
     <div className="container flex justify-space-between align-items-center padding-vert-tiny">
-      <NavLink to="/" className="logo h2 no-margin"><span>dev</span><span>Gaid</span>
-        <span className="relative">
-          <svg className="logo__compass absolute" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" strokeLinejoin="round" strokeMiterlimit="1.414">
-            <path d="M10 0c5.52 0 10 4.48 10 10s-4.48 10-10 10S0 15.52 0 10 4.48 0 10 0zm0 3.154c3.78 0 6.846 3.067 6.846 6.846 0 3.78-3.067 6.846-6.846 6.846-3.78 0-6.846-3.067-6.846-6.846 0-3.78 3.067-6.846 6.846-6.846z" fill="#d16c4b" />
-            <path d="M14.023 5.977l-5.62 2.426-2.426 5.62 5.62-2.426 2.426-5.62zm-3.077 3.077c.522.522.522 1.37 0 1.892-.522.522-1.37.522-1.892 0-.522-.522-.522-1.37 0-1.892.522-.522 1.37-.522 1.892 0z" fill="#d16c4b" />
-          </svg>
-        </span>
-      </NavLink>
+      <NavLink to="/" className="logo" />
       <nav className="main-navigation">
         <ul className="menu flex justify-content-center">
           <li><NavLink to="/styleguide" activeClassName="link-active">Styleguide</NavLink></li>
