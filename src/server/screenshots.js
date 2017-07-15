0d35d60058e639b8db5a4db69df6811f65bb574c
Kim Kwanka <kimkwanka@gmail.com>
2017-07-15 10:09:24 +0200

    Improve screenshot functionality + add latest screenshots.

--- src/server/screenshots.js
+++ src/server/screenshots.js
@@ -1,15 +1,44 @@
 const webshot = require('webshot');
 const lessons = require('./models/corelessons');
 
-let count = 0;
-Object.keys(lessons).forEach((lessonId) => {
-  if (lessons[lessonId].externalSource !== '') {
-    webshot(lessons[lessonId].externalSource, `./src/client/assets/screenshots/${lessonId}.jpeg`, (err) => {
-      if (err) { console.log(err); return; }
+const allLessonIds = Object.keys(lessons);
 
+const errors = [];
+let count = 0;
+
+const options = {
+  renderDelay: 2000,
+  timeOut: 3000,
+};
+
+const takeScreenshot = (lessonIds) => {
+  if (lessonIds.length > 0) {
+    const lessonId = lessonIds.pop();
+    const lesson = lessons[lessonId];
+    const extSrc = lesson.externalSource;
+    let res = lesson.resources ? lesson.resources[0][1] : '';
+    if (res === undefined) {
+      res = '';
+    }
+    const url = extSrc || res;
+    webshot(url, `./src/client/assets/screenshots/${lessonId}.jpg`, options, (err) => {
       count += 1;
-      console.log(lessons[lessonId].externalSource, count);
+
+      if (err) {
+        console.log('Couldn\'t take screenshot of', lessonId, url);
+        errors.push(lessonId);
+      } else {
+        console.log(`${lessonId}.jpg`);
+      }
+      takeScreenshot(lessonIds);
+    });
+  } else {
+    console.log('Successfully created', count, 'screenshots.');
+    console.log('Unsuccessful: ', errors.length);
+    errors.forEach((errLessonId) => {
+      console.log(errLessonId);
     });
   }
-});
+};
 
+takeScreenshot(allLessonIds);
