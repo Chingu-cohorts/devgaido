dbecc6ec29b6cfe8b40c38cff1cab51cb584dafd
Kim Kwanka <kimkwanka@gmail.com>
2017-07-20 02:31:27 +0200

    Fix 2x FontAwesome.css issue

--- src/server/reactRoutes.jsx
+++ src/server/reactRoutes.jsx
@@ -44,7 +44,7 @@ const renderPage = (match, store) => {
       <meta name="theme-color" content="#ffffff">
       ${cssFile}
       <link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,400,700" rel="stylesheet">
-      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     </head>
     <body>
       <div id="root">${reactMarkup}</div>
