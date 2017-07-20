e485eb685996a34eda827df2e6cf2ab32c3c3835
Kim Kwanka <kimkwanka@gmail.com>
2017-07-20 18:29:55 +0200

    Remove / update footer links.

--- src/client/pages/shared/Footer.jsx
+++ src/client/pages/shared/Footer.jsx
@@ -18,15 +18,15 @@ const Footer = () => (
       </div>
       <div className="flex">
         <Link to="/about" className="uppercase margin-right-small">About</Link>
-        <Link to="/" className="uppercase margin-right-small">Terms</Link>
+        {/* <Link to="/" className="uppercase margin-right-small">Terms</Link> */}
         <Link to="/disclaimer" className="uppercase margin-right-small">Disclaimer</Link>
-        <Link to="/" className="uppercase margin-right-small">Privacy</Link>
+        {/* <Link to="/" className="uppercase margin-right-small">Privacy</Link> */}
         <Link to="/contact" className="uppercase margin-right-small">Contact</Link>
       </div>
       <div className="flex h2">
         <a href="https://github.com/Chingu-cohorts/devgaido" target="_blank" rel="noopener noreferrer" className="uppercase margin-right-small"><i className="fa fa-github" /></a>
-        <a href="/" target="_blank" rel="noopener noreferrer" className="uppercase margin-right-small"><i className="fa fa-twitter" /></a>
-        <a href="/" target="_blank" rel="noopener noreferrer" className="uppercase margin-right-small"><i className="fa fa-youtube" /></a>
+        <a href="https://twitter.com/devgaido" target="_blank" rel="noopener noreferrer" className="uppercase margin-right-small"><i className="fa fa-twitter" /></a>
+        <a href="https://www.youtube.com/channel/UC3goUqdIGkmFYzi9mC9bSYQ" target="_blank" rel="noopener noreferrer" className="uppercase margin-right-small"><i className="fa fa-youtube" /></a>
       </div>
     </div>
   </footer>
