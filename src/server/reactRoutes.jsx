7034f691592ebdb995a2b4b5f07a035e076bfb34
Kim Kwanka <kimkwanka@gmail.com>
2017-07-26 18:13:42 +0200

    Move GA to head and user async snippet.

--- src/server/reactRoutes.jsx
+++ src/server/reactRoutes.jsx
@@ -45,22 +45,20 @@ const renderPage = (match, store) => {
       ${cssFile}
       <link href="https://fonts.googleapis.com/css?family=Fira+Sans:200,400,700" rel="stylesheet">
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+      <!-- Google Analytics -->
+      <script>
+      window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+      ga('create', 'UA-103320988-1', 'auto');
+      ga('send', 'pageview');
+      </script>
+      <script async src='https://www.google-analytics.com/analytics.js'></script>
+      <!-- End Google Analytics -->
     </head>
     <body>
       <div id="root">${reactMarkup}</div>
       <script>window.__INITIAL_STATE__ = ${JSON.stringify(store.getState()).replace(/</g, '\\u003c')};</script>
       <script src="${webRoot}/vendor.bundle.js"></script>
       <script src="${webRoot}/client.bundle.js"></script>
-      <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-103320988-1', 'auto');
-        ga('send', 'pageview');
-
-      </script>
     </body>
   </html>
   `;
