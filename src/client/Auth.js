ca0b4bfc288c9090e970c71b128f63f5acdbf98f
Kim Kwanka <kimkwanka@gmail.com>
2017-07-31 22:05:19 +0200

    Add popup.

--- src/client/Auth.js
+++ src/client/Auth.js
@@ -2,7 +2,6 @@ import auth0js from 'auth0-js';
 
 export default class Auth {
   constructor(auth0) {
-    console.log(auth0)
     this.auth0 = new auth0js.WebAuth({
       domain: auth0.domain,
       clientID: auth0.clientID,
@@ -13,6 +12,14 @@ export default class Auth {
   }
 
   login() {
-    this.auth0.authorize();
+    console.log('Login!')
+    this.auth0.popup.authorize({
+      redirectURI: 'http://localhost:8080/callback',
+    }, (err, data) => {
+      console.log('Am I ever called? T_T')
+      console.log('CALLBACK???', data, err);
+    });
+    console.log('After Login!')
+    // this.auth0.authorize();
   }
 }
