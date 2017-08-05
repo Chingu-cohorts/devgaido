19aef4203bc22db9e1b3e5edba09c1028fab0bd9
Jim Medlock <jdmedlock@gmail.com>
2017-08-05 16:36:18 -0500

    [DO NOT USE]
    Add contributor formatting to About page

--- src/client/pages/About/Contributors.jsx
+++ src/client/pages/About/Contributors.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LazyLoad from 'react-lazyload';
+
+const LoadingPlaceholder = () => (
+  <div className="loading__placeholder home-img__loading-spinner loading-spinner border-round" />
+);
+
+const ContributorImage = ({ avatarURL }) => (
+  <div>
+    <LazyLoad height={350} once placeholder={<LoadingPlaceholder />}>
+      <img className="border-round" src={avatarURL} alt="" />
+    </LazyLoad>
+  </div>
+);
 
 const Contributors = ({ contributors }) => {
+  const left = 'l';
+  const right = 'r';
+  const horizontalPosition = right;
+
   return (
-    <div className="results margin-vertical-big">
-      <div className="flex flex-wrap margin-vertical-big justify-space-around">
-        {contributors.map(aContributor =>
-          <h4 key={aContributor.login}>{aContributor.login}</h4>,
-        )}
-      </div>
+    <div className="container">
+      {contributors.map(aContributor =>
+        <section className="flex margin-top-huge">
+          <ContributorImage avatarURL={aContributor.avatar} />
+          <div className="flex-1 margin-left-small margin-top-small">
+            <h1 className="c-secondary bold">
+              <a href={aContributor.html} target="_blank" rel="noopener noreferrer">
+                {aContributor.login} </a>
+            </h1>
+          </div>
+        </section>,
+      )}
     </div>
   );
 };
