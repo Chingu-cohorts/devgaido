3114eeb3401707c77f23bbf95b572fefeeb2eb43
Nick Papasavvas <nikospapasavvas@gmail.com>
2017-08-19 07:46:44 +0300

    Add modal

--- src/client/pages/shared/Modal.jsx
+++ src/client/pages/shared/Modal.jsx
@@ -10,7 +10,7 @@ const Modal = () => (
   <div className="modal" style={{ position: `fixed`, backgroundColor: 'rgba(0,0,0,0.8)', top: 0, left: 0, height:'100%', width: '100%', display:'flex' }}>
     <div className="modal-container" 
     style={{ backgroundColor:'#fff',border:'1px solid #000', width:'50vw', maxWidth:'400px', margin: 'auto', padding:20,  borderRadius:5 }}>
-      <p> I love cats</p>
+      <p> I love all cats</p>
       <button onClick={() => toggleModal()}>Close the modal</button>
     </div>
   </div>
