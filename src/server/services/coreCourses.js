87c8478ce1aa73a81b8fc0740d4ebb2727ea5538
jdmedlock <jdmedlock@gmail.com>
2017-09-07 18:55:10 -0500

    Update comments

--- src/server/services/coreCourses.js
+++ src/server/services/coreCourses.js
@@ -4,21 +4,7 @@ import CoreCourses from '../models/corecourses.json';
  * Core Courses Model
  *
  * This module contains functions implementing model layer functionality
- * for the application. The JSON file referenced by this module has the
- * following structure:
- *
- * {
- *  "course-identifier" : { <-- Unique path identifier. Max of 16 chars.
- *    "name": "...",        <-- Short course name
- *    "description": "...", <-- Course description must define the objective
- *                              of the course and what the user can expect to
- *                              gain from it.
- *    "lessonIds": [        <-- Array of lesson ids included in the course
- *      "...",              <-- Lesson identifier
- *    ],
- *    "version": "1.0.0"    <-- Semantic version to track changes
- *  },
- * }
+ * for the application.
  */
 
 /*
@@ -42,7 +28,6 @@ const getExpectedAttributes = () => courseAttributes;
 /**
  * Extract a specific course and its details from the Core Courses
  *
- * @param {String} courseId - Unique course identifier
  * @returns {String[]} - JSON object containing attributes of the course or
  * undefined if no match found
  */
--- src/server/services/coreLessons.js
+++ src/server/services/coreLessons.js
@@ -4,35 +4,7 @@ import CoreLessons from '../models/corelessons.json';
  * Core Lessons Model
  *
  * This module contains functions implementing model layer functionality
- * for the application. The JSON file referenced by this module has the
- * following structure:
- *
- * {
- *  "path-identifier" : {          <-- Unique lesson identifier. Max of 16 chars.
- *    "source": "...",             <-- Textual definition of the lessons origin (e.g. P1XT)
- *    "name": "...",               <-- Short lesson name
- *    "description": "...",        <-- Lesson description. This should describe
- *                                     the lesson, as well as what knowledge will
- *                                     be provided to the user.
- *    "type": "...",               <-- Lesson type categorizes the medium used to
- *                                     transmit the information (e.g. reading, video, etc.)
- *    "instructions": "...",       <-- For "type": "Project" this describes what
- *                                     is expected of the user
- *    "resources":[["resource-description","resource-url"],...],
- *                                 <-- For "type": "Project" this defines relevant
- *                                     supplemental material
- *    "subjects: ["..."]..,        <-- An array of subject names this lesson is
- *                                     associated with.
- *    "externalSource": "...",     <-- Defines the url of the lesson
- *    "estimatedTime": "short|medium|long", <-- Defines the estimated amount of
- *                                     time required to complete the lesson.
- *                                     Acceptable values are:
- *                                     - short: 4 hours or less
- *                                     - medium: 16 hours or less
- *                                     - long: >16 hours
- *    "version": "1.0.0"           <-- Semantic version to track changes
- *  },
- * }
+ * for the application.
  */
 
 /*
@@ -62,7 +34,6 @@ const getExpectedAttributes = () => lessonAttributes;
 /**
  * Extract a specific lesson and its details from the Core Lessons
  *
- * @param {String} lessonId - Unique lesson identifier
  * @returns {String[]} - JSON object containing attributes of the lesson
  */
 const getLesson = lessonId => CoreLessons[lessonId];
