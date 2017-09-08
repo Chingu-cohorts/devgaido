ec150512fbdc10e42259988ceb69be51f1bdcf2f
Nick Papasavvas <nikospapasavvas@gmail.com>
2017-09-08 18:52:09 +0300

    add messages

--- src/client/pages/shared/Modal.jsx
+++ src/client/pages/shared/Modal.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
-
 import actions from '../../actions';
-
 const { toggleModal } = actions;
+
+const messages = [
+  'Next stop Mars!!',
+  'You are a star, you are a star, you are a super-star!!',
+  'Uuuh you are just warming up!!',
+  'Now this is what I call madskillz!!',
+  'You are so H-O-T!!',
+  'Are you Dan Abramov??',
+  'Google I am coming!!',
+  'Kim is that you training??',
+  'Next stop Hackathon!!'
+]
+
+const getRandomMessage = () => messages[Math.floor(Math.random() * messages.length)];
 
 const Modal = () => (
   <div className="modal" >
     <div className="modal-container">
-      <p> I love all cats</p>
+      <p style={{fontSize:20, color:'red'}}> { getRandomMessage() }</p>
       <div className="rating" style={{fontSize: 32}}>
         <span onClick={() => {
           console.log("5")
